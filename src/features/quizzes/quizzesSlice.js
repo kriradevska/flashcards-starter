9b3d13011bce0086ffcf85347beb3a118ab16e96
Kristina Radevska <kriradevska@gmail.com>
2022-10-31 20:31:41 +0100

    state add for quizzes

---
+++ src/features/quizzes/quizzesSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const quizzesSlice = createSlice({
+    name: 'quizzes',
+    initialState: {
+        quizzes: {}
+    },
+    reducers: {
+        addQuiz: (state, action) => {
+            const { id, name, topicId, cardIds} = action.payload;
+            state.quizzes[id] = {
+                id: id, 
+                name: name,
+                topicId: topicId,
+                cardIds: cardIds
+            }
+        }
+    }
+});
+
+export const selectQuizzes = state = state.quizzes.quizzes;
+
+export const {
+    addQuiz
+} = quizzes.action;
+
+export default quizzesSlice.reducer;
