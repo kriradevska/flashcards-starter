8cab2bca2ec702b009e24ce872285d8a3da3eab1
Kristina Radevska <kriradevska@gmail.com>
2022-10-31 18:21:09 +0100

    state edit

--- src/features/topics/topicSlice.js
+++ src/features/topics/topicSlice.js
@@ -1,23 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export default topicsSlice = createSlice({
+const topicsSlice = createSlice({
     name: 'topics',
     initialState: {
         topics: {}
     },
     reducers: {
         addTopic: (state, action) => {
-            return [
-                ...state,
-                {
+            const { id, name, icon } = action.payload;
+            state.topics[id] = {
                     id: action.payload.id,
                     name: action.payload.name,
                     icon: action.payload.icon,
                     quizIds: [] 
-                }
-            ]
+            };
         }
     }
 })
 
 export const selectTopics = state => state.topics;
+
+export const {
+    addTopic
+} = topicsSlice.actions;
+
+export default topicsSlice.reducer;
