feca0a6a4b20b076c9422f7c32685274b59fca87
Kristina Radevska <kriradevska@gmail.com>
2022-11-01 17:20:32 +0100

    cards state add and store update

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import topicsReducer from '../features/topics/topicSlice.js';
 import quizzesReducer from '../features/quizzes/quizzesSlice';
+import cardsReducer from '../features/cards/cardsSlice';
 
 export default configureStore({
   reducer: {
     topics: topicsReducer,
-    quizzes: quizzesReducer
+    quizzes: quizzesReducer,
+    cards: cardsReducer
   },
 });
